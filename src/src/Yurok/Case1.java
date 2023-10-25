ed8ce22405032dfe34c27dd5df25742a63128fd7
jinyr1128 <jinyr1128@gmail.com>
2023-10-25 15:18:30 +0900

    Case1 2023-10-25

--- src/src/Yurok/Case1.java
+++ src/src/Yurok/Case1.java
@@ -1,12 +1,25 @@
 package Yurok;
 
+import java.util.Scanner;
+
 public class Case1 {
-    public void gameStart(int randomNum){
-        //여기에 작성해 주세요
 
+    public static void gameStart(int randomNum) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자를 맞춰보세요!");
+
+        while (true) {
+            System.out.print("입력: ");
+            int userInput = Integer.parseInt(scanner.nextLine());
+
+            if (randomNum < userInput) {
+                System.out.println("Down");
+            } else if (randomNum > userInput) {
+                System.out.println("Up");
+            } else {
+                System.out.println("Correct!");
+                break;
+            }
+        }
     }
-
-
 }
-
-
