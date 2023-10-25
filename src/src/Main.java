3e7433a24af9a7fca99a0ee2a294a4497f421351
c1 <rysian7096@gmail.com>
2023-10-25 14:36:29 +0900

    siwon

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,6 +3,7 @@ import base.Case1;
 public class Main {
     public static void main(String[] args) {
         Case1 case1 = new Case1();
+        System.out.printf("hi");
 
         case1.gameStart(3);
 
