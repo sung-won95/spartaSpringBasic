7d8006888df698cb69e9bba5d9440b319d624605
m2bi <cjunh4972@naver.com>
2023-10-25 15:14:08 +0900

    game make

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,10 +1,8 @@
-import base.Case1;
+import JH.Case1;
 
 public class Main {
     public static void main(String[] args) {
         Case1 case1 = new Case1();
-
         case1.gameStart(3);
-
     }
 }
