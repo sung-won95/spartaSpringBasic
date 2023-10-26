a24af78f95e8c00cd1a474a7728e345cbcf77270
hyeon9won <mhw3901@gmail.com>
2023-10-26 09:41:03 +0900

    1일차 과제

--- src/src/hyeongwon/Case1.java
+++ src/src/hyeongwon/Case1.java
@@ -1,9 +1,23 @@
-package base;
+package hyeongwon;
+
+import java.util.Scanner;
 
 public class Case1 {
-    public void gameStart(int randomNum){
-        //여기에 작성해 주세요
+    public void gameStart(int randomNum) {
+        Scanner scanner = new Scanner(System.in);
+        int userInput = 0;
+        do {
+            System.out.println("숫자를 예측하세요.");
+            userInput = scanner.nextInt();
+            if (randomNum < userInput) {
+                System.out.println("DOWN");
+                System.out.println("다시 입력하세요.");
+            } else if (randomNum > userInput) {
+                System.out.println("UP");
+                System.out.println("다시 입력하세요.");
+            }
+            } while (userInput != randomNum) ;
+            System.out.println("GOOD");
+            System.out.println("정답입니다!");
+        }
     }
-}
-
-
