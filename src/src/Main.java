3d08dad9a42e8c7df2be10b07b7c905a1312688b
Mayst1232 <48579987+Mayst1232@users.noreply.github.com>
2023-10-27 19:44:28 +0900

    Delete src/src directory

--- src/src/Main.java
+++
@@ -1,9 +0,0 @@
-import kyujeong.Case1;
-
-public class Main {
-    public static void main(String[] args) {
-        Case1 case1 = new Case1();
-        int randomNumber = (int)(Math.random() * 1001);
-        case1.gameStart(randomNumber);
-    }
-}
