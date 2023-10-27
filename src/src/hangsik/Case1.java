898978a8389e3917acb42bf93b837e2b3d174774
kinghangsik <hgt9107@naver.com>
2023-10-27 16:40:54 +0900

    과제 제출합니다.

--- src/src/hangsik/Case1.java
+++ src/src/hangsik/Case1.java
@@ -1,8 +1,30 @@
-package base;
+package hangsik;
+
+import java.util.Scanner;
 
 public class Case1 {
     public void gameStart(int randomNum){
         //여기에 작성해 주세요
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 0;
+       for (;a<10;){
+            Integer userInput = Integer.parseInt(scanner.nextLine());
+            if(randomNum < userInput) {
+                System.out.println("DOWN");
+            } else if (randomNum > userInput) {
+                System.out.println("UP");
+            } else {
+                System.out.println("GOOD");
+                break;
+            }
+            a = a+1;
+        }
+
+//       for(1;2;3){
+//           4
+//       }
+//       1 > 2 확인 > 4 > 3 > 2확인
     }
 }
 
