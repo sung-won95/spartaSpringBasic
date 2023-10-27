68a2e4a04b91def4ed3ee7d86c801e67a99a9cb0
Xtraa777 <mazino0588@gmail.com>
2023-10-27 13:48:10 +0900

    junyeong Case1

---
+++ src/src/junyeong/Case1.java
@@ -0,0 +1,24 @@
+package junyeong;
+
+import java.util.Scanner;
+
+public class Case1 {
+    public void gameStart(int randomNum) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (; ; ) {
+            Integer userInput = Integer.parseInt(scanner.nextLine());
+            if (randomNum < userInput) {
+                System.out.println("DOWN!");
+            } else if (randomNum > userInput) {
+                System.out.println("UP!");
+            } else {
+                System.out.println("GOOD :)");
+                break;
+            }
+        }
+
+    }
+}
+
+
