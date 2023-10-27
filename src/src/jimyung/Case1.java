789a26a7e5311c0c922f1b5396b31575a197fbea
jimyung0418 <wjdwlaud0418@naver.com>
2023-10-27 11:25:26 +0900

    과제 제출

--- src/src/jimyung/Case1.java
+++ src/src/jimyung/Case1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Case1 {
     public void gameStart(int randomNum){
-        //여기에 작성해 주세요
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
