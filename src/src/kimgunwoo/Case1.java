335c3dd2e5a3fc595fc1a5501ecbd78c7e989f24
guncow0603 <guncow0603@gmail.com>
2023-10-30 17:04:53 +0900

    첫 과제

--- src/src/kimgunwoo/Case1.java
+++ src/src/kimgunwoo/Case1.java
@@ -1,9 +1,25 @@
 package kimgunwoo;
 
+import java.util.Scanner;
+
 public class Case1 {
     public void gameStart(int randomNum){
-        //여기에 작성해 주세요
+    Scanner scanner = new Scanner(System.in);
+    int a = 0;
+        do{
+            Integer userInput = Integer.parseInt(scanner.nextLine());
 
+            if (randomNum < userInput) {
+                System.out.println("DOWN");
+
+            } else if (randomNum > userInput) {
+                System.out.println("UP");
+            } else {
+                System.out.println("GOOD");
+                break;
+            }
+            a = a + 1 ;
+        }while (a<0);
     }
 
 
